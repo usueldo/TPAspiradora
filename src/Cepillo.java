2ff33a945eef6b81a92c91480c5c453b56f8ca76
Uriel <usueldo@gmail.com>
2020-11-16 22:03:39 -0300

    se quito indicador 'sucio' al cepillo

--- src/Cepillo.java
+++ src/Cepillo.java
@@ -1,35 +1,23 @@
 public class Cepillo {
-	boolean estaSucio=false;
-	int cantidadDeBarridos=0;
-	
-	
-	public boolean estaSucio() {
-		boolean estado=false;
-		if (cantidadDeBarridos==5){
-			
-			estado=true;
-		}
-		return estado;
-	}
+	int cantidadDeBarridos = 0;
 
+	public boolean estaSucio() {
+		return cantidadDeBarridos >= 5;
+	}
 
 	public void limpiarZona(Zona zona) {
-		
+
 		if (this.estaSucio()) {
 			System.out.print("El cepillo esta sucio\n");
-			
-			
+
 		} else {
 			zona.limpiar();
-			this.cantidadDeBarridos+=1;//Limpia zona y aumenta el cepillo en cantidad de barrido
+			this.cantidadDeBarridos += 1;// Limpia zona y aumenta el cepillo en cantidad de barrido
 		}
 	}
-	
-		
-	public void limpiarManualmente(){
-		this.estaSucio=false;//pone el cepillo sucio en false
-		this.cantidadDeBarridos=0;//resetea la cantidad de barridos
-		
-		}
-	
+
+	public void limpiarManualmente() {
+		this.cantidadDeBarridos = 0;// resetea la cantidad de barridos
+	}
+
 };
